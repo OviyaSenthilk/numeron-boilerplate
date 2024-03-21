d542106cebbe62b7ee875842351e5797c00ce553
OviyaSenthilk <pji.oviya.s@kalvium.community>
2024-03-21 18:40:48 +0530

    score.js

--- score.js
+++ score.js
@@ -1,1 +1,10 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let scoreBoard=document.getElementById('score-board');
+
+document.getElementById('play-again-button').addEventListener('click',setTimeAndPlayAgian);
+
+function setTimeAndPlayAgian(){
+    window.location="game.html";
+}
+
+scoreBoard.innerHTML=sessionStorage.getItem("score");
